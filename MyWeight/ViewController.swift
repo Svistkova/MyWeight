1fe17b2ce5827edf00dea56ba55e561c3098316e
Marina Svistkova <svmrin1@gmail.com>
2020-03-31 13:58:46 +0300

    Initial Commit

---
+++ MyWeight/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyWeight
+//
+//  Created by Marina Svistkova on 31.03.2020.
+//  Copyright © 2020 Marina Svistkova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
